99011c6e868af6cb633997c7206cbf3fd4db9850
h-core <ajlusardi@gmail.com>
2015-01-31 22:37:51 -0500

    no

--- Golf.hs
+++ Golf.hs
@@ -16,23 +16,27 @@ localMaxima _ = []
 --Identify all unique values in the integer array (with nub), and produce their counts.
 --Make sure all missing values get an empty bar
 -- uniquevalues :: [Integer] -> [(Integer,Int,Int)]
-uniquevalues :: (Enum t, Ord t) => [t] -> [(t, Int, t)]
-uniquevalues n = [(a,c,max')
+uniquevalues :: (Num t, Enum t, Ord t) => [t] -> [(t, Int, t)]
+uniquevalues vs = [(a,c,max')
   | a <- [min' .. max'],
-  let c  = (length $ filter (==a) n)]
-    where max' = maximum n
-          min' = minimum n
+  let c  = (length $ filter (==a) vs)]
+    where max' = maximum vs
+          min' = minimum vs
+
+-- maxinlist [] = []
+-- maxinlist (v:vs) = (length $ filter (==v) vs): maxinlist vs
 
 --Render the bar to be a string of '*' representing the count.
 --Under each bar print the axis value
 -- renderbar :: (Integer,Int,Int) -> String
-renderbar :: Show a => (a, Int, Int) -> [Char]
-renderbar (a,c,d) = replicate (d-c) ' ' ++ replicate c '*' ++ "=" ++ show a
-
+-- Problem with rendering only up to 9 is I need to put ... I don't know...
+renderbar :: (Int, Int, Int) -> String
+renderbar (a,c,d) = replicate (d-c) ' ' ++ replicate c '*' ++ spacing a '=' ++ spacing (d-a) ' ' ++ show a ++  []
+  where spacing x = replicate (length (show x))
 -- histogram :: [Integer] -> String
 -- Doesn't render right over 9?
 -- Also returning an IO () instead of a string obviously...
-histogram vs = mapM_ putStrLn $ transpose $ map renderbar $ uniquevalues vs
+histogram vs = foldl1 (++) $ map (++"\n") $ transpose $ map renderbar $ uniquevalues vs
 
 
 
