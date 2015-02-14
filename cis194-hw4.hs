f722fa404b9342e2e93f533cf2bdbddac1d92fb2
h-core <ajlusardi@gmail.com>
2015-02-14 18:44:44 -0500

    weebles wobble but they dont fall down

--- cis194-hw4.hs
+++ cis194-hw4.hs
@@ -34,18 +34,22 @@ splitHalf x = splitAt (div (length x) 2) x
 --      else Node n+1 right a left
 
 --IF I BREAK THE SUMMING INTO A SEPARATE FUNCTION THEN I CAN RETURN THE TUPLE I WANT WITH LEFT AND RIGHT LENGTHS
-treeLen :: (Eq a) => Tree a -> Integer
-treeLen (Node _ l _ r) = left + right
-  where
-    left = if l == Leaf then 0 :: Integer else 1 + treeLen l
-    right = if r == Leaf then 0 :: Integer else 1 + treeLen r
+-- treeLen :: (Eq a) => Tree a -> Integer
+-- treeLen (Node _ l _ r) = left + right
+--   where
+--     left = if l == Leaf then 0 :: Integer else 1 + treeLen l
+--     right = if r == Leaf then 0 :: Integer else 1 + treeLen r
 
-addNode :: (Eq a) => a -> Tree a -> Tree a
-addNode value tree@(Node _ l v r)
-    | odd n = (Node n (addNode value l) v r)
-    | otherwise = (Node n l v (addNode value r))
-  where n = treeLen tree
-        left = if l == Leaf then Leaf else
+-- addNode :: (Eq a) => a -> Tree a -> Tree a
+-- addNode value tree@(Node n l v r)
+--     | l == Leaf || r == Leaf = (Node n+1 Leaf value Leaf)
+--     |
+--     | otherwise =
+--   where nl = treeLen l
+--         nr = treeLen r
+
+-- foldTree :: [a] -> Tree a
+-- foldTree = foldr (\x acc@(Node n left value right) ->
 
 --Produce balanced binary tree of a list using foldr.
 -- foldTree :: [a] -> Tree a
@@ -58,8 +62,17 @@ addNode value tree@(Node _ l v r)
 --     right = snd split
 --     node = length left - 1
 
-xor :: [Bool] -> Bool
-xor x = odd $ foldr (\x -> if x==True then (+1) else (+0)) 0 x
+-- xor :: [Bool] -> Bool
+-- xor x = odd $ foldr (\x -> if x==True then (+1) else (+0)) 0 x
 
+xor' :: [Bool] -> Bool
+xor' = foldr (\x acc -> (x || acc) && not (x && acc)) False
 
+map' :: (a ->b) ->[a] ->[b]
+map' f = foldr (\x acc -> f x : acc) []
 
+-- myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs = foldr (\x acc -> ((x `f` acc) `f`)) base xs
+--
+f1 :: a -> a -> a
+f1 x y = x
