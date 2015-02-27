266aad1fef5ffdab204fe08ff1cb9cc1f46ca710
h-core <ajlusardi@gmail.com>
2015-02-26 21:51:53 -0500

    omg it works and i even know why now

--- cis194-hw5/Calc.hs
+++ cis194-hw5/Calc.hs
@@ -20,13 +20,27 @@ evalStr x
     where expr = parseExp Lit Add Mul x
 
 class Expr a where
-mul, add :: ExprT -> ExprT -> ExprT
-lit :: Integer -> ExprT
+  mul, add :: a -> a -> a
+  lit :: Integer -> a
 
 instance Expr ExprT where
-mul a b = (Mul a b)
-add a b = (Add a b)
-lit a = (Lit a)
+  mul a b = (Mul a b)
+  add a b = (Add a b)
+  lit a = (Lit a)
+
+instance Expr Integer where
+  mul a b = a * b
+  add a b = a + b
+  lit a = a
+
+instance Expr Bool where
+  lit a
+    | a <= 0 = False
+    | otherwise = True
+  mul a b = a && b
+  add a b = a || b
+
+-- instance Expr Integer where
 
 -- evalStr :: String -> Maybe Integer
 -- evalStr x = maybe Nothing eval (parseExp Lit Add Mul x)
