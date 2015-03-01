3153424449f0fd4cdd42a5145455d53ee8b0450b
h-core <ajlusardi@gmail.com>
2015-03-01 14:49:34 -0500

    instances are like magic

--- cis194-hw5/Calc.hs
+++ cis194-hw5/Calc.hs
@@ -1,4 +1,8 @@
-import ExprT
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+-- import qualified StackVM as S
+import StackVM
+import qualified ExprT as E
 import Parser
 import Data.Maybe
 --module Calc where
@@ -8,25 +12,25 @@ import Data.Maybe
 -- eval :: ExprT -> a
 
 -- instance ExprT where
-eval :: ExprT -> Integer
-eval (Lit a) = a
-eval (Add a b) = (eval a) + (eval b)
-eval (Mul a b) = (eval a) * (eval b)
+eval :: E.ExprT -> Integer
+eval (E.Lit a) = a
+eval (E.Add a b) = (eval a) + (eval b)
+eval (E.Mul a b) = (eval a) * (eval b)
 
 evalStr :: String -> Maybe Integer
 evalStr x
   | isNothing expr = Nothing
   | otherwise = Just $ eval $ fromJust expr
-    where expr = parseExp Lit Add Mul x
+    where expr = parseExp E.Lit E.Add E.Mul x
 
 class Expr a where
   mul, add :: a -> a -> a
   lit :: Integer -> a
 
-instance Expr ExprT where
-  mul a b = (Mul a b)
-  add a b = (Add a b)
-  lit a = (Lit a)
+instance Expr E.ExprT where
+  mul a b = (E.Mul a b)
+  add a b = (E.Add a b)
+  lit a = (E.Lit a)
 
 instance Expr Integer where
   mul a b = a * b
@@ -57,12 +61,19 @@ instance Expr Mod7 where
   add (Mod7 a) (Mod7 b) = lit (mod (add a b :: Integer) 7)
   lit a = (Mod7 a)
 
+-- newtype Program = P [StackExp] deriving (Show)
+instance Expr Program where
+  mul a b = a ++ b ++ [Mul]
+  add a b = a ++ b ++ [Add]
+  lit a = [PushI a]
+
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
 testInteger = testExp :: Maybe Integer
 testBool = testExp :: Maybe Bool
 testMinMax = testExp :: Maybe MinMax
 testMod7 = testExp :: Maybe Mod7
+testStackVM = testExp :: Maybe Program
 
 -- testInteger = testExp :: Maybe Integer
 -- instance Expr Integer where
