9c566fdf26f4f58634d0c702252b19f07d45cf52
h-core <ajlusardi@gmail.com>
2015-03-13 20:05:55 -0400

    fun times with haskells

--- cis194-hw5/Calc.hs
+++ cis194-hw5/Calc.hs
@@ -67,6 +67,9 @@ instance Expr Program where
   add a b = a ++ b ++ [Add]
   lit a = [PushI a]
 
+compile :: String -> Maybe Program
+compile s = parseExp lit add mul s
+
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
 testInteger = testExp :: Maybe Integer
